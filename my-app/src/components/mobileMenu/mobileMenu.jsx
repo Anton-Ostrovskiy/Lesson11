276accd2cb67ebddff447e7be83b43b8e8777d80
unknown <ostrovskiy.98@mail.ru>
2023-11-06 23:18:24 +0300

    mobile 2

--- my-app/src/components/mobileMenu/mobileMenu.jsx
+++ my-app/src/components/mobileMenu/mobileMenu.jsx
@@ -1,44 +1,44 @@
 import "./style.scss";
-const MobileMenu = ({active, setActive}) => {
+const MobileMenu = ({ active, setActive }) => {
   return (
     <nav className={active ? "menu active" : "menu"}>
-        <ul className="list">
-          <li className="item">
-            <a className="link" href="#!">
-              Home
-            </a>
-          </li>
-          <li className="item">
-            <a className="link" href="#!">
-              About Us
-            </a>
-          </li>
-          <li className="item">
-            <a className="link" href="#!">
-              Services
-            </a>
-          </li>
-          <li className="item">
-            <a className="link" href="#!">
-              Projects
-            </a>
-          </li>
-          <li className="item">
-            <a className="link" href="#!">
-              Team
-            </a>
-          </li>
-          <li className="item">
-            <a className="link" href="#!">
-              Career
-            </a>
-          </li>
-          <li className="item">
-            <a className="link" href="#!">
-              Contact Us
-            </a>
-          </li>
-        </ul>
+      <ul className="list">
+        <li className="item" onClick={() => setActive(!active)}>
+          <a className="link" href="#!">
+            Home
+          </a>
+        </li>
+        <li className="item" onClick={() => setActive(!active)}>
+          <a className="link" href="#!">
+            About Us
+          </a>
+        </li>
+        <li className="item" onClick={() => setActive(!active)}>
+          <a className="link" href="#!">
+            Services
+          </a>
+        </li>
+        <li className="item" onClick={() => setActive(!active)}>
+          <a className="link" href="#!">
+            Projects
+          </a>
+        </li>
+        <li className="item" onClick={() => setActive(!active)}>
+          <a className="link" href="#!">
+            Team
+          </a>
+        </li>
+        <li className="item" onClick={() => setActive(!active)}>
+          <a className="link" href="#!">
+            Career
+          </a>
+        </li>
+        <li className="item" onClick={() => setActive(!active)}>
+          <a className="link" href="#!">
+            Contact Us
+          </a>
+        </li>
+      </ul>
     </nav>
   );
 };
